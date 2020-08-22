2ce8f2e750e4b2afe7eb8738149030e38afcb98f
Devansh3712 <devanshamity@gmail.com>
2020-08-22 12:51:00 +0530

    added password for exit

--- skoolpy.py
+++ skoolpy.py
@@ -160,7 +160,7 @@ def showTimetable(name):
 		if a=="CSG":
 			CSG={"Monday":['12-B','12-B',0,0,'12-C','12-C',0,0],"Tuesday":[0,0,'12-C','12-C','12-A','12-A',0,0],"Wednesday":['12-C','12-C','12-A','12-A',0,0,'12-B','12-B'],"Thursday":['12-B','12-B',0,0,'12-A','12-A','12-C','12-C'],"Friday":[0,0,'12-A','12-A','12-B','12-B',0,0]}
 			for i in CSG:
-				if i==datetime.now().strftime("%A"):
+				if i==datetime.now().strftime("%A") and (datetime.now().strftime("%A")!="Saturday" or datetime.now().strftime("%A")!="Sunday"):
 					a=1
 					for j in CSG[i]:
 						if j!=0:
@@ -168,10 +168,13 @@ def showTimetable(name):
 						else:
 							print("Period {} is free".format(a))
 						a=a+1
+				else:
+					print("Today is a weekend, timetable not available")
+					break
 		elif a=="PLD":
 			PLD={"Monday":[1,1,1,1,0,0,1,1],"Tuesday":[1,1,1,1,0,0,0,0],"Wednesday":[1,1,0,0,1,1,0,0],"Thursday":[1,1,1,1,0,0,1,1],"Friday":[0,0,0,0,1,1,1,1]}
 			for i in PLD:
-				if i==datetime.now().strftime("%A"):
+				if i==datetime.now().strftime("%A") and (datetime.now().strftime("%A")!="Saturday" or datetime.now().strftime("%A")!="Sunday"):
 					a=1
 					for j in PLD[i]:
 						if j!=0:
@@ -179,10 +182,13 @@ def showTimetable(name):
 						else:
 							print("Period {} is free".format(a))
 						a=a+1
+				else:
+					print("Today is a weekend, timetable not available")
+					break
 		elif a=="MVK":
 			MVK={"Monday":[1,1,0,0,0,0,1,1],"Tuesday":[0,0,1,1,0,0,1,1],"Wednesday":[1,1,1,1,0,0,0,0],"Thursday":[1,1,1,1,0,0,1,1],"Friday":[1,1,0,0,1,1,1,1]}
 			for i in MVK:
-				if i==datetime.now().strftime("%A"):
+				if i==datetime.now().strftime("%A") and (datetime.now().strftime("%A")!="Saturday" or datetime.now().strftime("%A")!="Sunday"):
 					a=1
 					for j in MVK[i]:
 						if j!=0:
@@ -190,10 +196,13 @@ def showTimetable(name):
 						else:
 							print("Period {} is free".format(a))
 						a=a+1
+				else:
+					print("Today is a weekend, timetable not available")
+					break
 		elif a=="CSHG":
 			CSHG={"Monday":[1,1,0,0,0,0,1,1],"Tuesday":[1,1,0,0,1,1,0,0],"Wednesday":[0,0,1,1,0,0,1,1],"Thursday":[1,1,0,0,0,0,1,1],"Friday":[1,1,1,1,1,1,0,0]}
 			for i in CSHG:
-				if i==datetime.now().strftime("%A"):
+				if i==datetime.now().strftime("%A") and (datetime.now().strftime("%A")!="Saturday" or datetime.now().strftime("%A")!="Sunday"):
 					a=1
 					for j in CSHG[i]:
 						if j!=0:
@@ -201,10 +210,13 @@ def showTimetable(name):
 						else:
 							print("Period {} is free".format(a))
 						a=a+1
+				else:
+					print("Today is a weekend, timetable not available")
+					break
 		elif a=="ESB":
 			ESB={"Monday":[1,1,0,0,1,1,1,1],"Tuesday":[1,1,1,1,0,0,0,0],"Wednesday":[1,1,1,1,0,0,0,0],"Thursday":[0,0,1,1,0,0,1,1],"Friday":[0,0,1,1,0,0,1,1]}
 			for i in ESB:
-				if i==datetime.now().strftime("%A"):
+				if i==datetime.now().strftime("%A") and (datetime.now().strftime("%A")!="Saturday" or datetime.now().strftime("%A")!="Sunday"):
 					a=1
 					for j in ESB[i]:
 						if j!=0:
@@ -212,6 +224,9 @@ def showTimetable(name):
 						else:
 							print("Period {} is free".format(a))
 						a=a+1
+				else:
+					print("Today is a weekend, timetable not available")
+					break
 		else:
 			print("Name not found in record, try capitalizing initials of your name")
 	else:
@@ -262,7 +277,8 @@ while True:
 			checkTeacherStatus(name)
 			print()
 		elif tin=="6":
-			name=input("Enter name:")
+			name=input("Enter name: ")
+			print()
 			showTimetable(name)
 			print()
 		elif tin=="8":
